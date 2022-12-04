900d10a7bc93ee46492f265d16b8627a5794f665
Piotr1103 <manjudamin@gmail.com>
2022-12-04 12:52:56 +0800

    讓程式具有交互功能

--- main.py
+++ main.py
@@ -4,6 +4,17 @@ from BookmarkCopier import BookmarkCopier
 #test = BookmarkCopier('./demo/A.pdf','./demo/B.pdf',(21,258))
 
 #在原有頁數後面插入新頁數
-test = BookmarkCopier('./demo/Vue核心篇.pdf','./demo/Vue核心篇-已融合.pdf',None)
+# test = BookmarkCopier('./demo/Vue核心篇.pdf','./demo/Vue核心篇-已融合.pdf',None)
+
+src = input("請輸入書籤列來源檔案路徑：")
+dst = input("請輸入目的地檔案路徑：")
+offsets = input("被刪除的頁數，以空格隔開，沒有則輸入N：")
+if not offsets in ("N", "n"):
+	try:
+		offtup = tuple([int(i) for i in offsets.split()])
+	except:
+		print("錯誤!請確定輸入是否正確。", offtup, type(offtup))
+
+test = BookmarkCopier(src, dst, offtup)
 
 test.setbooklines()
